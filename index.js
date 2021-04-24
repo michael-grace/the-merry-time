07eec129aed51cf5929b38bf4ce93ed5d0cf1d4b
michael-grace <michael.dp.grace@outlook.com>
2021-04-24 16:45:51 +0100

    Async Anger

--- index.js
+++ index.js
@@ -13,33 +13,7 @@ const port = process.env.PORT | 8080;
 const storage = new Storage();
 var bucket;
 
-if (process.env.GCLOUD_STORAGE_BUCKET) {
-    bucket = storage.bucket(process.env.GCLOUD_STORAGE_BUCKET);
-
-    bucket.getFiles((err, files) => {
-        if (err) {
-            throw err;
-        }
-        files.forEach(async(element) => {
-            if (element.name === "scores.json") {
-                await element.download({ destination: "/tmp/scores.json" });
-            }
-        });
-    });
-}
-
 var appData;
-
-if (!existsSync("/tmp/scores.json")) {
-    copyFileSync("scores.json", "/tmp/scores.json");
-}
-
-readFile("/tmp/scores.json", (err, data) => {
-    if (err) {
-        throw err;
-    }
-    appData = JSON.parse(data);
-});
 
 // No, this doesn't get it to send back a 500
 const writeback = async() => {
@@ -54,42 +28,73 @@ const writeback = async() => {
     }
 };
 
-app.set("view engine", "pug");
-app.set("views", "views");
+async function main() {
+    if (process.env.GCLOUD_STORAGE_BUCKET) {
+        console.log("Cloud Bucket Exists");
+        bucket = storage.bucket(process.env.GCLOUD_STORAGE_BUCKET);
+        try {
+            console.log("Downloading Data");
+            await bucket
+                .file("scores.json")
+                .download({ destination: "/tmp/scores.json" });
+            console.log("Downloaded Data");
+        } catch (e) {
+            console.log("Failed Downlaoding Data");
+            throw e;
+        }
+    }
 
-app.get("/data", (_, res) => {
-    res.json(appData);
-});
+    if (!existsSync("/tmp/scores.json")) {
+        console.log("Creating New File");
+        copyFileSync("scores.json", "/tmp/scores.json");
+    }
 
-app.get("/", (_, res) => {
-    res.render("index", {
-        scores: Object.keys(appData.scores).map((elem) => {
-            return { name: elem, score: appData.scores[elem] };
-        }),
+    readFile("/tmp/scores.json", (err, data) => {
+        if (err) {
+            throw err;
+        }
+        appData = JSON.parse(data);
     });
-});
 
-app.get("/add/:name/:confirm?", (req, res) => {
-    let name = req.params.name;
-    let confirm = req.params.confirm;
+    app.set("view engine", "pug");
+    app.set("views", "views");
 
-    if (name in appData.scores) {
-        appData.scores[name]++;
-        res.status(200).send("Added to: " + name);
-        writeback();
-        return;
-    }
+    app.get("/data", (_, res) => {
+        res.json(appData);
+    });
 
-    if (confirm === "yes") {
-        appData.scores[name] = 1;
-        res.status(200).send("Now in Competition: " + name);
-        writeback();
-        return;
-    }
+    app.get("/", (_, res) => {
+        res.render("index", {
+            scores: Object.keys(appData.scores).map((elem) => {
+                return { name: elem, score: appData.scores[elem] };
+            }),
+        });
+    });
 
-    res.status(400).send("Confirmation Needed. Correct Person? " + name);
-});
+    app.get("/add/:name/:confirm?", (req, res) => {
+        let name = req.params.name;
+        let confirm = req.params.confirm;
 
-app.listen(port, () => {
-    console.log("Started on Port:", port);
-});
+        if (name in appData.scores) {
+            appData.scores[name]++;
+            res.status(200).send("Added to: " + name);
+            writeback();
+            return;
+        }
+
+        if (confirm === "yes") {
+            appData.scores[name] = 1;
+            res.status(200).send("Now in Competition: " + name);
+            writeback();
+            return;
+        }
+
+        res.status(400).send("Confirmation Needed. Correct Person? " + name);
+    });
+
+    app.listen(port, () => {
+        console.log("Started on Port:", port);
+    });
+}
+
+main();
